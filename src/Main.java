7d18ab4e057589575f07ce185b396a2095fab519
inkaenfu <inkaenfu@gmail.com>
2024-03-18 08:17:21 +0100

    Promenny

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-    }
+public class Main {
+    private static final int SIZE = 3;
+    private static final char EMPTY = '-';
+    private static char[][] board = new char[SIZE][SIZE];
+    private static char currentPlayer = 'X';
+
 }
