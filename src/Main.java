8684e7f38139d4bc175856e8866a9e9518b33000
inkaenfu <inkaenfu@gmail.com>
2024-03-18 08:22:35 +0100

    nova metoda na print pole

--- src/Main.java
+++ src/Main.java
@@ -6,4 +6,30 @@ public class Main {
     private static char[][] board = new char[SIZE][SIZE];
     private static char currentPlayer = 'X';
 
+    public static void main(String[] args) {
+        printBoard();
+
+        // Hra
+        Scanner scanner = new Scanner(System.in);
+        boolean gameEnded = false;
+        scanner.close();
+    }
+
+
+    private static void printBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
 }
+
+
+
